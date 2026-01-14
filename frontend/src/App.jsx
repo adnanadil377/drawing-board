b682b619f8b31ce2286bbdaa9f698617a5b88daf
adnanadil377 <139038605+adnanadil377@users.noreply.github.com>
2026-01-14 08:50:40 +0530

    Update API_URL to new endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import DrawingControls from './DrawingControls';
 import GameEndScreen from './GameEndScreen';
 
-const API_URL = 'https://boxapop492.pythonanywhere.com';
+const API_URL = 'https://sojoxe5015.pythonanywhere.com';
 const POLLING_INTERVAL = 2500;
 
 function App() {
